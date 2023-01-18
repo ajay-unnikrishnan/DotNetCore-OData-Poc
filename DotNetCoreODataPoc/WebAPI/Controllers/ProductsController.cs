32ac232e2bbe8f9d14391404d32cd93acb0a3910
Ajaykrishnan Unnikrishnan <ajaykrishnan.unnikrishnan@boeing.com>
2023-01-18 11:05:35 +0530

    Initial checkin

---
+++ DotNetCoreODataPoc/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                Id = index,
+                Name = "Product" + index
+
+            }).ToList();
+        }
+    }
+}
